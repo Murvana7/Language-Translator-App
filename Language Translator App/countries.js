06dab26866145c6921f4a61c278d6e1b985f0737
Damuu <d_murvanidze@cu.edu.ge>
2026-01-28 18:49:54 +0400

    Add files via upload

---
+++ Language Translator App/countries.js
@@ -0,0 +1,101 @@
+const countries = {
+  "am-ET": "Amharic",
+  "ar-SA": "Arabic (Saudi Arabia)",
+  "ar-EG": "Arabic (Egypt)",
+  "ar-AE": "Arabic (United Arab Emirates)",
+  "ar-MA": "Arabic (Morocco)",
+
+  "zh-CN": "Chinese (Simplified, China)",
+  "zh-TW": "Chinese (Traditional, Taiwan)",
+  "zh-HK": "Chinese (Traditional, Hong Kong)",
+
+  "en-US": "English (United States)",
+  "en-GB": "English (United Kingdom)",
+  "en-CA": "English (Canada)",
+  "en-AU": "English (Australia)",
+  "en-IN": "English (India)",
+  "en-NZ": "English (New Zealand)",
+  "en-ZA": "English (South Africa)",
+
+  "fr-FR": "French (France)",
+  "fr-CA": "French (Canada)",
+  "fr-BE": "French (Belgium)",
+  "fr-CH": "French (Switzerland)",
+  "fr-LU": "French (Luxembourg)",
+
+  "es-ES": "Spanish (Spain)",
+  "es-MX": "Spanish (Mexico)",
+  "es-AR": "Spanish (Argentina)",
+  "es-CL": "Spanish (Chile)",
+  "es-CO": "Spanish (Colombia)",
+  "es-PE": "Spanish (Peru)",
+  "es-US": "Spanish (United States)",
+
+  "pt-PT": "Portuguese (Portugal)",
+  "pt-BR": "Portuguese (Brazil)",
+
+  "de-DE": "German (Germany)",
+  "de-AT": "German (Austria)",
+  "de-CH": "German (Switzerland)",
+  "de-LU": "German (Luxembourg)",
+
+  "it-IT": "Italian (Italy)",
+  "it-CH": "Italian (Switzerland)",
+
+  "nl-NL": "Dutch (Netherlands)",
+  "nl-BE": "Dutch (Belgium)",
+
+  "ru-RU": "Russian (Russia)",
+  "uk-UA": "Ukrainian (Ukraine)",
+  "ka-GE": "Georgian (Georgia)",
+
+  "ja-JP": "Japanese (Japan)",
+  "ko-KR": "Korean (South Korea)",
+  "th-TH": "Thai (Thailand)",
+  "vi-VN": "Vietnamese (Vietnam)",
+
+  "hi-IN": "Hindi (India)",
+  "bn-IN": "Bengali (India)",
+  "bn-BD": "Bengali (Bangladesh)",
+  "ta-IN": "Tamil (India)",
+  "te-IN": "Telugu (India)",
+  "ml-IN": "Malayalam (India)",
+  "mr-IN": "Marathi (India)",
+  "gu-IN": "Gujarati (India)",
+  "pa-IN": "Punjabi (India)",
+  "ur-PK": "Urdu (Pakistan)",
+  "ur-IN": "Urdu (India)",
+
+  "tr-TR": "Turkish (Turkey)",
+  "el-GR": "Greek (Greece)",
+  "he-IL": "Hebrew (Israel)",
+  "sv-SE": "Swedish (Sweden)",
+  "no-NO": "Norwegian (Norway)",
+  "da-DK": "Danish (Denmark)",
+  "fi-FI": "Finnish (Finland)",
+  "pl-PL": "Polish (Poland)",
+  "cs-CZ": "Czech (Czechia)",
+  "sk-SK": "Slovak (Slovakia)",
+  "hu-HU": "Hungarian (Hungary)",
+  "ro-RO": "Romanian (Romania)",
+  "bg-BG": "Bulgarian (Bulgaria)",
+  "sr-RS": "Serbian (Serbia)",
+  "hr-HR": "Croatian (Croatia)",
+  "sl-SI": "Slovenian (Slovenia)",
+
+  "et-EE": "Estonian (Estonia)",
+  "lv-LV": "Latvian (Latvia)",
+  "lt-LT": "Lithuanian (Lithuania)",
+
+  "id-ID": "Indonesian (Indonesia)",
+  "ms-MY": "Malay (Malaysia)",
+
+  "sw-KE": "Swahili (Kenya)",
+  "sw-TZ": "Swahili (Tanzania)",
+
+  "fa-IR": "Persian (Iran)",
+  "ps-AF": "Pashto (Afghanistan)",
+  "pa-PK": "Punjabi (Pakistan)",
+
+  // ...add more as needed
+};
